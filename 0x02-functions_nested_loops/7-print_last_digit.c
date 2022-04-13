25b747c3491840fc7a2e4e03d80b3ce5731552d3
Benjamin3443 <101414331+Benjamin3443@users.noreply.github.com>
2022-04-13 11:36:10 +0100

    Update 7-print_last_digit.c

--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -9,15 +9,15 @@ int print_last_digit(int n)
 {
 	int a;
 
-        if (n < 0)
-
+	if (n < 0)
 		n = -n;
-        a = n % 10;
 
-        if (a < 0)
+	a = n % 10;
+
+	if (a < 0)
 		a = -a;
 
-        _putchar(a + '0');
+	_putchar(a + '0');
 
-        return (a);
+	return (a);
 }
