fe67a440c5d3baa3ca2b8ebcea966cdfb815a696
Benjamin3443 <101414331+Benjamin3443@users.noreply.github.com>
2022-04-14 20:09:35 +0100

    Update main.h

--- 0x03-debugging/main.h
+++ 0x03-debugging/main.h
@@ -1,7 +1,8 @@
 #ifndef MAIN_H
 #define MAIN_H
 #include <stdio.h>
-void positive_or_negative(int i);
 int largest_number(int a, int b, int c);
+void positive_or_negative(int i);
+void print_remaining_days(int month, int day, int year);
 #endif
 /* MAIN_H */
