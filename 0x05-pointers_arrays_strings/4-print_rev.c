e16f190704da3fbf45adee89fe6472ba140c47c3
Benjamin Eruvieru <benjamineruvieru@gmail.com>
2022-04-20 06:20:41 -0700

    Created 4-print_rev.c

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ *print_rev - to print a string in reverse
+ *@s: pointer to string
+ */
+void print_rev(char *s)
+{
+	int i;
+
+	while (s[i] != '\0')
+	{
+		i++;
+	}
+	while (i > 0)
+	{
+		putchar(s[i]);
+	}
+	putchar("\n");
+}
