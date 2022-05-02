4b23758260851ee1ab4976ebbe5be540d2bb98dd
Benjamin Eruvieru <benjamineruvieru@gmail.com>
2022-05-02 12:58:28 -0700

    my my

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+/**
+  * main - Print name of program
+  * @argc: number of inputs
+  * @argv: array of inputs
+  * Return: 0 if true
+  */
+int main(int argc, char *argv[])
+{
+	printf("%s\n", argv[0]);
+	return (0);
+}
