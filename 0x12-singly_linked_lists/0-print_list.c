3ea7fdcbefde6f8a0e367419eb31ae673a4915cf
Benjamin3443 <101414331+Benjamin3443@users.noreply.github.com>
2022-05-25 22:35:00 +0100

    Update 0-print_list.c

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -1,12 +1,12 @@
 #include "lists.h"
 
 /**
- * print_listint - prints all the elements of a linked list
+ * print_list - prints all the elements of a linked list
  * @h: linked list of type listint_t to print
  *
  * Return: number of nodes
  */
-size_t print_listint(const listint_t *h)
+size_t print_list(const list_t *h)
 {
 	size_t num = 0;
 
