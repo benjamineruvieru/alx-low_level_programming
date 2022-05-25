639a7174f3f9147a8c4fabf6f48dcb0e7938d98f
Benjamin3443 <101414331+Benjamin3443@users.noreply.github.com>
2022-05-25 22:32:36 +0100

    Create 1-list_len.c

---
+++ 0x12-singly_linked_lists/1-list_len.c
@@ -0,0 +1,20 @@
+#include "lists.h"
+
+/**
+ * list_len - returns the number of elements in a linked lists
+ * @h: linked list of type list_t to traverse
+ *
+ * Return: number of nodes
+ */
+size_t list_len(const list_t *h)
+{
+	size_t num = 0;
+
+	while (h)
+	{
+		num++;
+		h = h->next;
+	}
+
+	return (num);
+}
