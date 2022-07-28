3e7167b72aa90b5685c780cf5de14f28405fb28d
Oghenekaro Eruvieru <benjamineruvieru@gmail.com>
2022-07-28 18:57:59 +0100

    first task

---
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -0,0 +1,31 @@
+#include "hash_tables.h"
+
+/**
+ * hash_table_create - creates a hash table
+ * @size: size of the hash table
+ *
+ * Return: pointer to the newly created hashtable
+ */
+
+hash_table_t *hash_table_create(unsigned long int size)
+{
+
+    hash_table_t *ht;
+
+    if (size == 0)
+        return (NULL);
+
+    ht = malloc(sizeof(hash_table_t));
+
+    if (ht == NULL)
+        return (NULL);
+
+    ht->size = size;
+
+    ht->array = calloc((size_t)ht->size, sizeof(hash_node_t *));
+
+    if (ht->array == NULL)
+        return NULL;
+
+    return ht;
+}
