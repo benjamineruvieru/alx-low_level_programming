d01c838d76fcdb70b818aea4600dee6b7c50ac39
Oghenekaro Eruvieru <benjamineruvieru@gmail.com>
2022-07-28 19:14:22 +0100

    betty fix

--- 0x1A-hash_tables/0-hash_table_create.c
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -8,19 +8,19 @@
  */
 hash_table_t *hash_table_create(unsigned long int size)
 {
-    hash_table_t *ht;
+	hash_table_t *ht;
 
-    if (size == 0)
-        return (NULL);
+	if (size == 0)
+		return (NULL);
 
-    ht = malloc(sizeof(hash_table_t));
-    if (!ht)
-        return (NULL);
+	ht = malloc(sizeof(hash_table_t));
+	if (!ht)
+		return (NULL);
 
-    ht->size = size;
-    ht->array = calloc((size_t)ht->size, sizeof(hash_node_t *));
-    if (ht->array == NULL)
-        return (NULL);
+	ht->size = size;
+	ht->array = calloc((size_t)ht->size, sizeof(hash_node_t *));
+	if (ht->array == NULL)
+		return (NULL);
 
-    return (ht);
+	return (ht);
 }
