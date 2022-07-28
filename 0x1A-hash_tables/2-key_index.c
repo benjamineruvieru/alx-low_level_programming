e2b927d93c2375cb1fb442aa08022d4601be98e5
Oghenekaro Eruvieru <benjamineruvieru@gmail.com>
2022-07-28 19:31:50 +0100

    task 3

---
+++ 0x1A-hash_tables/2-key_index.c
@@ -0,0 +1,19 @@
+/**
+ * key_index - a funciton that returns the index of a key
+ * @key: the key
+ * @size: size of the array of the hash table
+ *
+ * Returns: the index of the key
+ */
+
+unsigned long int key_index(const unsigned char *key, unsigned long int size)
+{
+	unsigned long int hash;
+
+	if (size == 0)
+		return (0);
+
+	hash = hash_djb2(key);
+
+	return (hash % size);
+}
