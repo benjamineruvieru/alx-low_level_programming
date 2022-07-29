0826d1069f0bef48214c7ec19654d281e28ae753
Oghenekaro Eruvieru <benjamineruvieru@gmail.com>
2022-07-29 15:52:35 +0100

    bug fix

--- 0x1A-hash_tables/hash_tables.h
+++ 0x1A-hash_tables/hash_tables.h
@@ -35,6 +35,20 @@ typedef struct hash_table_s
 	hash_node_t **array;
 } hash_table_t;
 
+hash_table_t *hash_table_create(unsigned long int size);
+unsigned long int hash_djb2(const unsigned char *str);
+unsigned long int key_index(const unsigned char *key, unsigned long int size);
+int hash_table_set(hash_table_t *ht, const char *key, const char *value);
+char *hash_table_get(const hash_table_t *ht, const char *key);
+void hash_table_print(const hash_table_t *ht);
+void hash_table_delete(hash_table_t *ht);
+
+void replace_value(hash_node_t **ht, const char *key, const char *value);
+int check_key(hash_node_t *ht, const char *key);
+hash_node_t *add_node(hash_node_t **head, const char *key, const char *value);
+void print_list(hash_node_t *h);
+void free_list(hash_node_t *head);
+
 /**
  * struct shash_node_s - Node of a sorted hash table
  *
@@ -72,18 +86,18 @@ typedef struct shash_table_s
 	shash_node_t *stail;
 } shash_table_t;
 
-hash_table_t *hash_table_create(unsigned long int size);
-unsigned long int hash_djb2(const unsigned char *str);
-unsigned long int key_index(const unsigned char *key, unsigned long int size);
-int hash_table_set(hash_table_t *ht, const char *key, const char *value);
-char *hash_table_get(const hash_table_t *ht, const char *key);
-void hash_table_print(const hash_table_t *ht);
-void hash_table_delete(hash_table_t *ht);
 shash_table_t *shash_table_create(unsigned long int size);
 int shash_table_set(shash_table_t *ht, const char *key, const char *value);
 char *shash_table_get(const shash_table_t *ht, const char *key);
 void shash_table_print(const shash_table_t *ht);
 void shash_table_print_rev(const shash_table_t *ht);
 void shash_table_delete(shash_table_t *ht);
+
+int replace_value_s(shash_node_t **ht, const char *key, const char *value);
+int check_key_s(shash_node_t *ht, const char *key);
+shash_node_t *add_node_s(shash_node_t **head,
+			 const char *key, const char *value);
+void insert_sort(shash_node_t *node, shash_table_t *ht);
+void free_list_s(shash_node_t *head);
 
 #endif
