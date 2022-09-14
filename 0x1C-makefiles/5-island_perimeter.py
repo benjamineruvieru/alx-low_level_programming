8485d85b5524d256d4760620c1ebcc5e8de7ffa4
Benjamin Eruvieru <benjamineruvieru@gmail.com>
2022-09-14 23:25:14 +0100

    Create 5-island_perimeter.py

---
+++ 0x1C-makefiles/5-island_perimeter.py
@@ -0,0 +1,27 @@
+#!/usr/bin/python3
+"""
+Technical interview preparation
+"""
+
+
+def island_perimeter(grid):
+    """Returns the perimeter of the island in grid."""
+    
+    perimeter = 0
+    pos = 0
+    for row in grid:
+        index = 0
+        for num in row:
+            if num == 1:
+                perimeter = perimeter + 4
+                if grid[pos+1][index] == 1:
+                    perimeter = perimeter - 1
+                if grid[pos][index+1] == 1:
+                    perimeter = perimeter - 1
+                if grid[pos-1][index] == 1:
+                    perimeter = perimeter - 1
+                if grid[pos][index-1] == 1:
+                    perimeter = perimeter - 1
+            index = index + 1
+        pos = pos + 1
+    return perimeter
