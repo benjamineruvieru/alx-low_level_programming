1a97538d55476ec1736a42d1cc40ff6a5a54ad2f
ben <benjamineruvieru@gmail.com>
2022-11-01 18:34:17 +0100

    fix

--- 0x1E-search_algorithms/0-linear.c
+++ 0x1E-search_algorithms/0-linear.c
@@ -1,11 +1,11 @@
 #include "search_algos.h"
 
 /**
- * @file 0-linear.c
+ *linear_search - Searches for a value in an array
+ *                 of integers using linear search.
  * @array: A pointer to the first element of the array to search
  * @size: The size of the array
  * @value: The value to search for
- * @author Benjamin Eruvieru (benjamineruvieru@gmail.com)
  */
 
 int linear_search(int *array, size_t size, int value)
